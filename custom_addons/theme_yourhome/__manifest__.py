b1a5f8ecbef5c9017326296db9580677953424b8
Luca Livrizzi (luli) <luca.eco1@hotmail.com>
2022-12-05 10:04:38 +0100

    first commit

---
+++ custom_addons/theme_yourhome/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'YourHome Theme',
+    'description': 'YourHome website theme',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [],
+    'images': [
+    ],
+    'snippet_lists': {
+    },
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/theme_yourhome2/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'YourHome2 Theme',
+    'description': 'YourHome website theme',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': ['views/header.xml',],
+    'images': [
+    ],
+    'snippet_lists': {
+    },
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
